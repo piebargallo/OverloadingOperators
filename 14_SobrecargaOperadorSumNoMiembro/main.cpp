f80dd86488b435c5fe3f7c879662e6d819f0e215
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-02-08 19:11:07 +0100

    Add files via upload

---
+++ 14_SobrecargaOperadorSumNoMiembro/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Punto
+{
+public:
+    Punto() = default;
+    Punto(double x, double y) : x(x), y(y) {};
+
+    // Declaración de la sobrecarga del operador + no miembro
+    friend inline Punto operator+(const Punto & p1, const Punto & p2);
+
+    void print() {
+        std::cout << "(" << x << ", " << y << ")\n";
+    }
+
+private:
+    double x{}, y{};
+};
+
+// Definición de la sobrecarga del operador + no miembro
+inline Punto operator+(const Punto& p1, const Punto& p2)
+{
+    return Punto(p1.x + p2.x, p1.y + p2.y);
+}
+
+int main()
+{
+    Punto p1(3, 4), p2(6, 2);
+    Punto pr{ p1 + p2 };
+    pr.print(); // (9, 6)
+    return 0;
+}
